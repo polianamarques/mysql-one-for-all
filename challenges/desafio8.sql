64cdcb7e4aa9c8cd6297cd73de314e4b82a996d4
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2023-05-09 15:48:41 -0300

    Requisito 08

---
+++ challenges/desafio8.sql
@@ -0,0 +1,7 @@
+SELECT art.artist_name, 
+      alb.name_album AS album 
+      FROM artist AS art
+	   INNER JOIN album AS alb
+       ON art.id_artist = alb.id_artist
+       WHERE art.artist_name = 'Elis Regina'
+       ORDER BY album;
